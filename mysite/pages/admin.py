8912bb859769c1447769b3c174137aea0f053941
David Ramirez <drami102@fiu.edu>
2020-10-19 14:27:45 -0400

    Modified mysite/pages/admin.py:  - Registered new models to admin site

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import User
+from .models import Group, GroupMember, Event, Attendee
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Group)
+admin.site.register(GroupMember)
+admin.site.register(Event)
+admin.site.register(Attendee)
