fa8065b97418f8af93304f4f1a21906a3f685857
David Ramirez <drami102@fiu.edu>
2020-10-20 12:08:01 -0400

    Modified mysite/pages/admin.py:  - Removed registering User, already handled by django

--- mysite/pages/admin.py
+++ mysite/pages/admin.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .models import Group, GroupMember, Event, Attendee
 # Register your models here.
 
-admin.site.register(User)
+# admin.site.register(User)
 admin.site.register(Group)
 admin.site.register(GroupMember)
 admin.site.register(Event)
