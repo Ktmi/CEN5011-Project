3dd2196a690f9d90b27a10221607c10859cac426
David Ramirez <drami102@fiu.edu>
2020-10-20 12:10:01 -0400

    Modified mysite/pages/models.py:  - Added on_delete=models.CASCADE to foreign keys  - Named unique constraints

--- mysite/pages/models.py
+++ mysite/pages/models.py
@@ -4,26 +4,29 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Group(models.Model):
-    id = models.ForeignKey(User, primary_key = True)
+    id = models.ForeignKey(User, primary_key = True, on_delete=models.CASCADE)
 
 class GroupMember(models.Model):
-    user_id = models.ForeignKey(User)
-    group_id = models.ForeignKey(Group)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields = ['user_id', 'group_id'])
+            models.UniqueConstraint(name = 'unique_Group_Member', fields = ['user_id', 'group_id'])
+        ]
+        indexes = [
+            models.Index(fields=['group_id', 'user_id'])
         ]
 
 class Event(models.Model):
-    host_id = models.ForeignKey(User)
+    host_id = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
 
 class Attendee(models.Model):
-    event_id = models.ForeignKey(Event)
-    attendee_id = models.ForeignKey(User)
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attendee_id = models.ForeignKey(User, on_delete=models.CASCADE)
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['event_id', 'attendee_id'])
+            models.UniqueConstraint(name = 'unique_Event_Attendee', fields=['event_id', 'attendee_id'])
         ]
         indexes = [
             models.Index(fields=['event_id', 'attendee_id'])
