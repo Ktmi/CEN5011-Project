80bdcfa36937c14b63e0e931299ed9f03ab41ece
David Ramirez <drami102@fiu.edu>
2020-10-26 13:31:36 -0400

    Modified mysite/mysite/urls.py:  - removed path to old signup page from patterns

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('find_event/', find_event_view, name='find_event'),
     path('contact/', contact_view, name='contact'),
     path('login/', login_view, name='login'),
-    path('sign_up/', sign_up_view, name='sign_up'),
     path('admin/', admin.site.urls),
 ]
