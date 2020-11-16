a45086a7b2490221c943948b293226cc278818b7
David Ramirez <drami102@fiu.edu>
2020-11-16 02:10:52 -0500

    Modified mysite/meeting/urls.py:  - Added pattern for MeetingView  - Added pattern for JoinMeetingView

--- mysite/meeting/urls.py
+++ mysite/meeting/urls.py
@@ -1,6 +1,8 @@
-from .views import CreateMeetingView
+from .views import CreateMeetingView, MeetingView, JoinMeetingView
 from django.urls import path
 
 urlpatterns = [
     path('create/', CreateMeetingView.as_view()),
+    path('<int:event_id>/', MeetingView.as_view()),
+    path('<int:event_id>/join/', JoinMeetingView.as_view()),
 ]
